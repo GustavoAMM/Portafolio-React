7cda654076c16e42fec5a01848bda7a9c8cde0b0
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-12 12:53:17 -0600

    fix structure project

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar";
 import Email from "./pages/Email";
 import Skills from './pages/Skills';
-import Sidebar from './pages/Sidebar';
-import Body from './pages/Body';
 import Projects from './pages/Projects'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="flex my-20">
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route path="/" element={<Body />} />
+          <Route path="/" element={<Home />} />
           <Route path="skills" element={<Skills />} />
           <Route path="projects" element={<Projects />} />
           <Route path="email" element={<Email />} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,4 @@
+function Home(){
+    return <h1 className="text-white">Hola home</h1>
+}
+export default Home
