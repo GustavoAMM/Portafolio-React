a485f9b3434bb5b99353a051e6045f136c273c90
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-12 11:45:38 -0600

    complete pag home

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Body from "./Body";
 import Sidebar from "./Sidebar";
 function App() {
   return (
-    <div className="flex ">
+      <div className="flex my-20">
       <div className="px-2 grid content-center">
         <Navbar></Navbar>
       </div>
-      <div className="px-2 w-3/4  grid content-center">
+      <div className="px-2 w-3/4 grid content-center">
         <Body></Body>
       </div>
       <div className="px-4 w-1/4 grid content-center">
         <Sidebar />
       </div>
     </div>
+    
   );
 }
 
