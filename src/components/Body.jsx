3ffc0d784c3cb36b91cf14be144cffa41d175821
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-13 15:48:24 -0600

    fix body home

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,42 +5,45 @@ import { SiPinboard } from "react-icons/si";
 
 function Body() {
   return (
-    <div className="rounded-3xl p-2 flex items-center" style={{ background: "#191724" }}>
+    <div className="rounded-3xl p-4 flex items-center" style={{ background: "#191724" }}>
+      {/* container img and description */}
+      <div className=" grid justify-items-center w-1/3 ">
         <img
           src="src/assets/icono.png"
-          className="rounded-full w-60 h-60 p-4"
+          className="rounded-full w-56 h-56"
         />
-        <div className="text-cente px-2 ">
-          <h1 className="text-white text-center font-mono font-bold text-3xl my-4">
-            Angel Montoya
-          </h1>
-          <ul className="my-10">
-            <li className="flex text-yellow-300">
-              <SiJavascript></SiJavascript>
-              <h1 className="px-2">Javascript developer</h1>
-            </li>
-            <li className="flex text-blue-400">
-              <GrReactjs></GrReactjs>
-              <h1 className="px-2">Fronteedn</h1>
-            </li>
-            <li className="flex text-red-600">
-              <SiNestjs></SiNestjs>
-              <h1 className="px-2">Backend</h1>
-            </li>
-            <li className="flex text-pink-300 italic">
-              <SiPinboard></SiPinboard>
-              <h1 className="px-2">"I create stuff sometimes"</h1>
-            </li>
-          </ul>
-          <p className="text-white my-2">
-            My name is Angel, I am a 20-year-old Mexican programmer who likes to
-            develop many more varied things that come from my imagination.
-          </p>
-          <p className="text-white mb-20">
-            I am very passionate about programming and cybersecurity, I am
-            fullstack but I prefer backend. I really like python and JavaScript.
-          </p>
-        </div>
+        <ul className="my-4">
+          <li className="flex text-yellow-400">
+            <SiJavascript></SiJavascript>
+            <h1 className="px-2">Javascript developer</h1>
+          </li>
+          <li className="flex text-cyan-400">
+            <GrReactjs></GrReactjs>
+            <h1 className="px-2">React frontend</h1>
+          </li>
+          <li className="flex text-red-600">
+            <SiNestjs></SiNestjs>
+            <h1 className="px-2">Nestjs backend</h1>
+          </li>
+          <li className="flex text-orange-200 italic">
+            <SiPinboard></SiPinboard>
+            <h1 className="px-2">"I create stuff sometimes"</h1>
+          </li>
+        </ul>
+      </div>
+      {/* Container title and paragraph */}
+      <div className="text-cente w-2/3 px-12 text-justify">
+        <h1 className="text-white text-center font-mono font-bold text-3xl my-4">
+          Angel <span style={{ color: "crimson" }}>Montoya</span>
+        </h1>
+        <p className="text-white mt-8 font-mono">
+          My name is Ángel, I am a 20-year-old Mexican programmer who likes to develop many different things that come from my imagination.
+        </p>
+        <br />
+        <p className="text-white font-mono">
+          I am very passionate about programming and cybersecurity, I am fullstack but I prefer backend. I really like Python and JavaScript.
+        </p>
+      </div>
     </div>
   );
 }
