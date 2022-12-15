1a040e2ba3a6909561af79d039920e04187bbf4a
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-14 20:32:20 -0600

    fix tags card projects

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,35 +4,39 @@ function Card() {
     return (
         <a href="">
             <div className="w-64 p-0 my-4 rounded-2xl card-main hover:scale-105">
-            <Image
-                src='https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg'
-                alt='astrounant'
-                objectFit='cover'
-                mx='auto'
-                className="rounded-t-2xl"
-            />
-            <div className="card-Body">
+                <Image
+                    src='https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg'
+                    alt='astrounant'
+                    objectFit='cover'
+                    mx='auto'
+                    className="rounded-t-2xl"
+                />
+                <div className="card-Body">
 
-            <HStack m='3' spacing='1'>
-                {['java', 'python'].map((item) => (
-                    <Tag key={item} variant="outline">
-                        {item}
-                    </Tag>
-                ))}
-            </HStack>
+                    <div className="card-tag flex mx-2">
+                        {["js", "python", "docker"].map((item) => (
+                            <div
+                                key={item}
+                                className="mt-2 mx-1 px-2 rounded-md text-sm bg-rose-200"
+                            >
+                                {item}
+                            </div>
 
-            <h1 className="card-title text-white font-bold">
-                Proyecto escolar UPQ
-            </h1>
-            <div className="container-description m-3 mb-2 text-white" >
-                <Text>
-                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus, doloribus iusto, dicta doloremque fugit natus, saepe distinctio quae sequi veritatis quis reiciendis porro expedita impedit nemo quaerat pariatur est!
-                </Text>
+                        ))}
+                    </div>
+
+                    <h1 className="card-title text-white font-bold">
+                        Proyecto escolar UPQ
+                    </h1>
+                    <div className="container-description px-2 m-3 text-white" >
+                        <Text>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus, doloribus iusto, dicta doloremque fugit natus, saepe distinctio quae sequi veritatis quis reiciendis porro expedita impedit nemo quaerat pariatur est!
+                        </Text>
+                    </div>
+                </div>
             </div>
-            </div>
-        </div>
         </a>
-        
+
     );
 }
 export default Card;
