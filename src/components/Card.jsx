7081bef7f83554dae9422d9d116d32f635d85f13
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-14 21:40:43 -0600

    add props functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
-import { Box, Heading, HStack, Image, Tag, Text, Button, Center } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import "./style/Card.css"
-function Card() {
+function Card({tag,title,description}) {
+    console.log(tag)
     return (
         <a href="">
             <div className="w-64 p-0 my-4 rounded-2xl card-main hover:scale-105">
@@ -14,7 +15,7 @@ function Card() {
                 <div className="card-Body">
 
                     <div className="card-tag flex mx-2">
-                        {["js", "python", "docker"].map((item) => (
+                        {tag.map((item) => (
                             <div
                                 key={item}
                                 className="mt-2 mx-1 px-2 rounded-md text-sm bg-rose-200"
@@ -24,14 +25,15 @@ function Card() {
 
                         ))}
                     </div>
+                    
 
                     <h1 className="card-title text-white font-bold">
-                        Proyecto escolar UPQ
+                        {title}
                     </h1>
-                    <div className="container-description px-2 m-3 text-white" >
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus, doloribus iusto, dicta doloremque fugit natus, saepe distinctio quae sequi veritatis quis reiciendis porro expedita impedit nemo quaerat pariatur est!
-                        </Text>
+                    <div className="container-description text-white" >
+                        <p>
+                          {description}
+                        </p>
                     </div>
                 </div>
             </div>
--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,19 +1,37 @@
-import "./style/carrusel.css"
-import Card from './Card'
-
+import "./style/carrusel.css";
+import Card from "./Card";
+import { Input } from "@chakra-ui/react";
 
 function Carrusel() {
   return (
     <div className="container-main rounded-3xl p-4 items-center">
-      <h1 className="text-white text-center font-mono font-bold text-3xl my-8">Proj<span style={{ color: "crimson" }}>ects</span></h1>
+      <h1 className="text-white text-center font-mono font-bold text-3xl my-4">
+        Proj<span style={{ color: "crimson" }}>ects</span>
+      </h1>
+      <Input placeholder="Categoria" className="text-white my-2" />
       <div className="container-card grid grid-cols-3 justify-items-center">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card
+          tag={["js", "java", "python"]}
+          title="Proyecto UPQ"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatem vel laborum, eum soluta reiciendis possimus at ratione in, et ut sit ipsam quis error odit officia ex? Cupiditate, quis."
+        />
+        <Card
+          tag={["Rust", "NestJs"]}
+          title="Task list"
+          description="Loremet ut sit ipsam quis error odit officia ex? Cupiditate, quis."
+        />
+        <Card
+          tag={["C++", "Arch", "Bash","Ubuntu"]}
+          title="Portafolio"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatem vel laborum, eum soluta"
+        />
+        <Card
+          tag={["C++"]}
+          title="Portafolio version 2 react"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatem vel laborum, eum soluta"
+        />
       </div>
     </div>
-
   );
 }
 export default Carrusel;
