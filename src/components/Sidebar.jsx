dcceac7aa8bbe5eb0cb57f2319432a54b55ea36d
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-15 12:18:20 -0600

    style home

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar() {
     <div className="grid content-around h-full">
       {/* About me */}
       <div className="rounded-3xl p-4 h-48 text-white " style={{ background: "#191724" }}>
-        <h1 className="text-center font-bold text-xl py-4">About me</h1>
+        <h1 className="text-white text-center font-mono font-bold text-2xl my-4">About <span style={{ color: "crimson" }}>me</span></h1>
         <div className="flex">
           <TiArrowRightOutline className="my-1 scale-150" style={{color:"crimson"}}/>
           <p className="px-2 texto-body">Age: 20</p>
@@ -23,9 +23,7 @@ function Sidebar() {
       </div>
       {/* CV */}
      { <div className="rounded-3xl p-4 h-36" style={{ background: "#191724" }}>
-        <h1 className="text-white text-center font-bold py-4 text-xl">
-          Curriculum
-        </h1>
+     <h1 className="text-white text-center font-mono font-bold text-2xl mt-2 mb-6">curri<span className="font-mono" style={{ color: "crimson" }}>culum</span></h1>
         <div className="flex justify-center ">
           <a href="#" >
             <TiDownloadOutline className="w-12 h-12 hover:w-14 hover:h-14 animate-bounce" style={{color:"crimson"}} />
