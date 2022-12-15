9b9bd642b3ddc88231fb7f7f6072d6b6a8393d77
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-14 19:50:05 -0600

    fix project's card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,8 @@ import { Box, Heading, HStack, Image, Tag, Text, Button, Center } from "@chakra-
 import "./style/Card.css"
 function Card() {
     return (
-        <div className="w-64 h-80 p-0 my-4 rounded-2xl card-main">
+        <a href="">
+            <div className="w-64 p-0 my-4 rounded-2xl card-main hover:scale-105">
             <Image
                 src='https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg'
                 alt='astrounant'
@@ -12,24 +13,26 @@ function Card() {
             />
             <div className="card-Body">
 
-            <HStack mt='1' spacing='1'>
+            <HStack m='3' spacing='1'>
                 {['java', 'python'].map((item) => (
                     <Tag key={item} variant="outline">
                         {item}
                     </Tag>
                 ))}
-
             </HStack>
-            <Heading mt='1' size='md' className="text-white">
-                Primer
-            </Heading>
-            <div className="container-description mt-1 text-white" >
+
+            <h1 className="card-title text-white font-bold">
+                Proyecto escolar UPQ
+            </h1>
+            <div className="container-description m-3 mb-2 text-white" >
                 <Text>
-                    Lorem ipsum dolor sit amet consectetur adip
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt possimus, doloribus iusto, dicta doloremque fugit natus, saepe distinctio quae sequi veritatis quis reiciendis porro expedita impedit nemo quaerat pariatur est!
                 </Text>
             </div>
             </div>
         </div>
+        </a>
+        
     );
 }
 export default Card;
