7e16aa7c1f58a5f4c1df4f0e4a0b3c61c39712e9
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-15 21:11:14 -0600

    add dinamic create card functionality

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,43 +1,39 @@
 import "./style/carrusel.css";
 import Card from "./Card";
 import { Input } from "@chakra-ui/react";
+import React from "react";
 
 function Carrusel() {
+  const data = [
+    {
+      id: 0,
+      link: "https://github.com/GustavoAMM/Portafolio-React",
+      banner: "https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg",
+      tag: ["java", "pyhton"],
+      title: "Portafolio React",
+      description: "Proyecto personal para poder practicar React y mostrar todos mis proyectos"
+    }
+  ];
+
   return (
     <div className="container-main rounded-3xl p-4 items-center">
       <h1 className="text-white text-center font-mono font-bold text-3xl my-4">
         Proj<span style={{ color: "crimson" }}>ects</span>
       </h1>
       <div className="grid justify-items-center">
-      <Input placeholder="Programming language" autoFocus={true} className="text-white focus:" width="400px"  />
+        <Input placeholder="Programming language" autoFocus={true} className="text-white focus:" width="400px" />
 
       </div>
       <div className="container-card grid grid-cols-3 justify-items-center mt-4">
-        <Card
-          tag={["js", "java", "python"]}
-          title="Proyecto UPQ"
-          description="Lorem "
-          banner='https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg'
-        />
-        
-        <Card
-          tag={["Rust", "NestJs"]}
-          title="Task list"
-          description="Proyecto personal creado con herramientas de react para poder aprender dicha tecnologia lore Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, neque et! Placeat numquam maiores illo modi facere quisquam eius tenetur laborum debitis suscipit rem consequuntur optio explicabo iure, laboriosam amet."
-          banner="https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg"
-        />
-        <Card
-          tag={["C++", "Arch", "Bash","Ubuntu"]}
-          title="Portafolio"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatem vel laborum, eum soluta"
-          banner="https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg"
-        />
-        <Card
-          tag={["C++"]}
-          title="Portafolio version 2 react"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptatem vel laborum, eum soluta"
-          banner="https://w.wallhaven.cc/full/1p/wallhaven-1p398w.jpg"
-        />
+        {
+          data.map((cita) => (
+            <div key={cita.id} >
+              <Card info={cita}>
+              </Card>
+            </div>
+
+          ))
+        }
       </div>
     </div>
   );
