90d4f8ec4173bf592f69e6d94a5887a5e3697e70
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-17 13:25:54 -0600

    fix style cv container

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar() {
       {/* About me */}
       <div className="rounded-3xl p-4 xl:h-48 sm:h-48 xl:w-60 sm:w-96 text-white " style={{ background: "#191724" }}>
         <h1 className="text-white text-center font-mono font-bold text-2xl my-4">About <span style={{ color: "crimson" }}>me</span></h1>
-        <div className="sm:text-xl xl:text-lg">
+        <div className="sm:text-xl xl:text-lg grid sm:justify-items-center xl:justify-items-start">
         <div className="flex">
           <TiArrowRightOutline className="my-1 scale-150" style={{ color: "crimson" }} />
           <p className="px-2 texto-body">Age: 20</p>
@@ -28,7 +28,7 @@ function Sidebar() {
         <h1 className="text-white text-center font-mono font-bold text-2xl mt-2 mb-6">Curri<span className="font-mono" style={{ color: "crimson" }}>culum</span></h1>
         <div className="grid content-center justify-items-center ">
           <a href="#" >
-            <TiDownloadOutline className="w-12 h-12 hover:w-14 hover:h-14 animate-bounce" style={{ color: "crimson" }} />
+            <TiDownloadOutline className="w-12 h-12 hover:w-14 hover:h-14 animate-bounce sm:m-4 xl:m-0" style={{ color: "crimson" }} />
           </a>
         </div>
       </div>
