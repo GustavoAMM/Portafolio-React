867847abad065cd7184f6bcc800724bbf564a87d
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-16 20:09:28 -0600

    add card error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -38,7 +38,30 @@ function Card({ info }) {
         );
     }else{
         return(
-        <h1 className="text-white">Projecto no creado :(</h1>);
+            <div className="w-60 justify-items-center my-2 rounded-2xl card-main hover:scale-105">
+            <Image
+                src="src/img/bannerError.jpg"
+                objectFit='cover'
+                className="rounded-t-2xl h-28 w-full"
+            />
+            <div className="card-Body">
+
+                <div className="card-tag flex justify-center mx-2">
+                        <div className="mt-2 mx-1 px-2 rounded-md text-sm bg-rose-200">
+                            Error
+                        </div>
+                </div>
+                <h1 className="card-title m-0 text-white text-center font-bold">
+                Project not found
+                </h1>
+                <div className="container-description text-center m-0 py-4 text-white" >
+                    <p>
+                    Incorrect or incomplete name
+                    </p>
+                </div>
+            </div>
+        </div>
+        )     
     }
     
 }
