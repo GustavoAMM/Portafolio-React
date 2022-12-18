9814d4f653bc613028925d332a33dca5ca0da34f
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-17 19:59:52 -0600

    add responsive style social

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,11 +2,11 @@ import "./style/Body.css"
 
 function Text() {
   return (
-    <div className="h-full rounded-3xl xl:grid sm:flex" style={{ background: "#191724" }}>
+    <div className="h-full xl:pt-0 sm:pt-0 pt-10 rounded-3xl xl:grid sm:flex" style={{ background: "#191724" }}>
        <h1 className="text-white text-center font-mono font-bold text-3xl xl:pt-10 xl:flex  sm:grid sm:content-center xl:justify-center sm:px-12 xl:px-0">
        Contact<span className="px-4" style={{ color: "crimson" }}>me</span>
         </h1>
-      <p className="text-white text-justify px-8 texto-body xl:pt-2 sm:py-4 text-lg">
+      <p className="text-white text-justify px-8 texto-body xl:pt-2 sm:py-4 py-4 text-lg">
         I'm seeking out opportunities to collaborate with
         companies / agencies / individuals, not just work for them. I want to bring
         my programming experience to the table where we can work together to
--- src/pages/Email.jsx
+++ src/pages/Email.jsx
@@ -2,12 +2,12 @@ import Social from "../components/Social";
 import Text from "../components/Text";
 function Email() {
   return (
-    <div className="xl:w-full sm:w-[750px] xl:px-8 sm:px-0">
-      <div className="xl:flex sm:grid gap-y-10">
+    <div className="w-full px-4 py-12">
+      <div className="xl:flex sm:grid grid gap-y-10">
         <div className="xl:w-4/5 sm:full xl:px-20 sm:px-0">
           <Social></Social>
         </div>
-        <div className="xl:w-96 sm:w-full xl:px-4 sm:px-0">
+        <div className="xl:w-96 sm:w-full xl:h-auto sm:h-auto h-[400px] xl:px-4 sm:px-0">
           <Text></Text>
         </div>
       </div>
