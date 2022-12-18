9a06e4b11c3021faa9ae6be9e3451656ade31969
Angel Montoya <j.gustavo.m245@gmail.com>
2022-12-17 20:09:06 -0600

    fix style slidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import "./style/Body.css"
 
 function Sidebar() {
   return (
-    <div className="xl:grid sm:flex flex xl:content-around sm:justify-around justify-around xl:h-full items-center">
+    <div className="xl:grid sm:flex grid xl:content-around sm:justify-around xl:h-full sm:h-[190px] h-[400px] items-center px-4">
       {/* About me */}
       <div className="rounded-3xl p-4 xl:h-48 sm:h-48 xl:w-60 sm:w-96 text-white " style={{ background: "#191724" }}>
         <h1 className="text-white text-center font-mono font-bold text-2xl my-4">About <span style={{ color: "crimson" }}>me</span></h1>
@@ -24,7 +24,7 @@ function Sidebar() {
         </div>
       </div>
       {/* CV */}
-      <div className="rounded-3xl xl:p-4 sm:py-6 p-4 xl:h-full sm:h-full h-full" style={{ background: "#191724" }}>
+      <div className="rounded-3xl xl:p-4 sm:py-6 p-4 xl:h-full sm:h-full h-[130px]" style={{ background: "#191724" }}>
         <h1 className="text-white text-center font-mono font-bold text-2xl mt-2 mb-6">Curri<span className="font-mono" style={{ color: "crimson" }}>culum</span></h1>
         <div className="grid content-center justify-items-center ">
           <a href="#" >
